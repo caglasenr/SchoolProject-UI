8272bdd0751118eccad88212cea708d8e8401c57
caglasenr <caglasener1729@gmail.com>
2024-06-11 19:48:49 +0300

    Bloc integrated

--- lib/view/favorites_page.dart
+++ lib/view/favorites_page.dart
@@ -1,13 +1,16 @@
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:edirne_gezgini_ui/database/temporary_database.dart';
-import 'package:edirne_gezgini_ui/model/accommodation.dart';
-import 'package:edirne_gezgini_ui/model/enum/base_place_category.dart';
+import 'package:edirne_gezgini_ui/bloc/favorites_bloc/favorites_bloc.dart';
+import 'package:edirne_gezgini_ui/bloc/favorites_bloc/favorites_event.dart';
+import 'package:edirne_gezgini_ui/bloc/favorites_bloc/favorites_state.dart';
+import 'package:edirne_gezgini_ui/service/favorite_service.dart';
 import 'package:edirne_gezgini_ui/widget/place_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:edirne_gezgini_ui/constants.dart' as constants;
+import 'package:edirne_gezgini_ui/model/dto/favorite_dto.dart';
+import 'package:edirne_gezgini_ui/model/enum/base_place_category.dart';
 
-import '../model/favorite.dart';
-import '../model/place.dart';
+import '../bloc/favorites_bloc/favorites_status.dart';
 
 class FavoritePage extends StatefulWidget {
   const FavoritePage({super.key});
@@ -17,120 +20,121 @@ class FavoritePage extends StatefulWidget {
 }
 
 class _FavoritePageState extends State<FavoritePage> {
-  List<Favorite> favoritePlaces = TemporaryDatabase()
-      .favorites
-      .where((favorite) => favorite.category == BasePlaceCategory.place)
-      .toList();
-  List<Favorite> favoriteAccommodations = TemporaryDatabase()
-      .favorites
-      .where((favorite) => favorite.category == BasePlaceCategory.accommodation)
-      .toList();
-
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width / 100;
     double height = MediaQuery.of(context).size.height / 100;
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
+
+    return BlocProvider(
+      create: (context) => FavoritesBloc(
+        favoriteService: context.read<FavoriteService>(),
+      )..add(GetFavoriteList()),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
           centerTitle: true,
           backgroundColor: Colors.white,
           scrolledUnderElevation: 0.0,
           title: const Text(
             "favorilerim",
             style: TextStyle(fontWeight: FontWeight.bold),
-          )),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(8),
-            child: AutoSizeText(
-              "Favori mekanlarım",
-              style: TextStyle(
-                  fontSize: 24,
-                  color: constants.primaryTextColor,
-                  fontWeight: FontWeight.bold),
-            ),
           ),
-
-          const SizedBox(
-            height: 16,
-          ),
-          //list places
-          SizedBox(
-            width: width * 100,
-            height: height * 65,
-            child: favoritePlacesListView(favoritePlaces, width*0.5, height*1),
-          ),
-
-          const Padding(
-            padding: EdgeInsets.all(8),
-            child: AutoSizeText(
-              "Favori konaklama yerlerim",
-              style: TextStyle(
-                  fontSize: 24,
-                  color: constants.primaryTextColor,
-                  fontWeight: FontWeight.bold),
-            ),
-          ),
-
-          const SizedBox(height: 16),
-
-          //list accommodations
-          SizedBox(
-            width: width * 100,
-            height: height * 65,
-            child: favoriteAccommodationsListView(favoriteAccommodations, width*0.5, height*1),
-          ),
-        ],
+        ),
+        body: BlocBuilder<FavoritesBloc, FavoritesState>(
+          builder: (context, state) {
+            if (state.favoritesStatus is GetFavoriteListPending) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state.favoritesStatus is GetFavoriteListSuccess) {
+              return ListView(
+                scrollDirection: Axis.vertical,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(8),
+                    child: AutoSizeText(
+                      "Favori mekanlarım",
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: constants.primaryTextColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  // List places
+                  SizedBox(
+                    width: width * 100,
+                    height: height * 65,
+                    child: favoritePlacesListView(state.favoriteList[BasePlaceCategory.place] ?? [], width * 0.5, height * 1),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8),
+                    child: AutoSizeText(
+                      "Favori konaklama yerlerim",
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: constants.primaryTextColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  // List accommodations
+                  SizedBox(
+                    width: width * 100,
+                    height: height * 65,
+                    child: favoriteAccommodationsListView(state.favoriteList[BasePlaceCategory.accommodation] ?? [], width * 0.5, height * 1),
+                  ),
+                ],
+              );
+            } else if (state.favoritesStatus is GetFavoriteListFailed) {
+              final failureStatus = state.favoritesStatus as GetFavoriteListFailed;
+              return Center(child: Text('Error: ${failureStatus.message}'));
+            }
+            return Container();
+          },
+        ),
       ),
     );
   }
 
-  Widget favoritePlacesListView(List<Favorite> favoritePlaces, double width, double height) {
+  Widget favoritePlacesListView(List<FavoriteDto> favoritePlaces, double width, double height) {
     return ListView.builder(
-        padding: const EdgeInsets.all(8),
-        scrollDirection: Axis.horizontal,
-        itemCount: favoritePlaces.length,
-        itemBuilder: (BuildContext context, int index) {
-
-          Place currentPlace = favoritePlaces[index].favoritePlace as Place;
-
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child:
-                PlaceCard(
-                    title: currentPlace.title,
-                    image: currentPlace.image,
-                    width: width,
-                    height: height,
-                    isVisited: false,
-                ),
-          );
-        });
+      padding: const EdgeInsets.all(8),
+      scrollDirection: Axis.horizontal,
+      itemCount: favoritePlaces.length,
+      itemBuilder: (BuildContext context, int index) {
+        var currentPlace = favoritePlaces[index];
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: PlaceCard(
+            title: currentPlace.favoritePlaceId,
+            image: "", // buraya tekrar bakılacaaakkkk!!!
+            width: width,
+            height: height,
+            isVisited: false,
+          ),
+        );
+      },
+    );
   }
 
-  Widget favoriteAccommodationsListView(List<Favorite> favoriteAccommodations, double width, double height) {
+  Widget favoriteAccommodationsListView(List<FavoriteDto> favoriteAccommodations, double width, double height) {
     return ListView.builder(
-        padding: const EdgeInsets.all(8),
-        scrollDirection: Axis.horizontal,
-        itemCount: favoriteAccommodations.length,
-        itemBuilder: (BuildContext context, int index) {
-
-          Accommodation currentAccommodation =
-              favoriteAccommodations[index].favoritePlace as Accommodation;
-
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: PlaceCard(
-                title: currentAccommodation.title,
-                image: currentAccommodation.image,
-                width: width,
-                height: height,
-                isVisited: false,
-            ),
-          );
-        });
+      padding: const EdgeInsets.all(8),
+      scrollDirection: Axis.horizontal,
+      itemCount: favoriteAccommodations.length,
+      itemBuilder: (BuildContext context, int index) {
+        var currentAccommodation = favoriteAccommodations[index];
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: PlaceCard(
+            title: currentAccommodation.favoritePlaceId,
+            image: "", // buraya tekrar bakılacaaaakkkk!!!!
+            width: width,
+            height: height,
+            isVisited: false,
+          ),
+        );
+      },
+    );
   }
 }
+
--- lib/view/hotels_page.dart
+++ lib/view/hotels_page.dart
@@ -1,11 +1,14 @@
+import 'package:edirne_gezgini_ui/bloc/hotels_bloc/hotels_bloc.dart';
+import 'package:edirne_gezgini_ui/bloc/hotels_bloc/hotels_event.dart';
+import 'package:edirne_gezgini_ui/bloc/hotels_bloc/hotels_state.dart';
+import 'package:edirne_gezgini_ui/service/accommodation_service.dart';
 import 'package:edirne_gezgini_ui/model/enum/base_place_category.dart';
 import 'package:edirne_gezgini_ui/view/place_details_page.dart';
 import 'package:flutter/material.dart';
-
-import '../database/temporary_database.dart';
-import '../model/accommodation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../widget/place_card.dart';
-
+import 'package:edirne_gezgini_ui/bloc/hotels_bloc/hotels_status.dart';
+import 'package:edirne_gezgini_ui/model/dto/accommodation_dto.dart';
 
 class HotelsPage extends StatefulWidget {
   const HotelsPage({super.key});
@@ -17,49 +20,75 @@ class HotelsPage extends StatefulWidget {
 class _HotelsPageState extends State<HotelsPage> {
   @override
   Widget build(BuildContext context) {
-    List<Accommodation> hotels = TemporaryDatabase().hotels;
     double width = MediaQuery.of(context).size.width / 100;
     double height = MediaQuery.of(context).size.height / 100;
-    return Scaffold(
+
+    return BlocProvider(
+      create: (context) => HotelsBloc(
+        accommodationService: context.read<AccommodationService>(),
+      )..add(GetHotelList()),
+      child: Scaffold(
         appBar: AppBar(
-            centerTitle: true,
-            title: const Text(
-              "Oteller",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            )),
-        body: ListView(
-          scrollDirection: Axis.horizontal,
-          children: [
-            //list hotels
-            SizedBox(
-              width: width * 100,
-              height: height * 65,
-              child: hotelsListView(hotels, width*1, height*1),
-            )
-          ],
-        ));
+          centerTitle: true,
+          title: const Text(
+            "Oteller",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: BlocBuilder<HotelsBloc, HotelsState>(
+          builder: (context, state) {
+            if (state.hotelListStatus is GetHotelListPending) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state.hotelListStatus is GetHotelListSuccess) {
+              return ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(
+                    width: width * 100,
+                    height: height * 65,
+                    child: hotelsListView(state.hotelList, width * 1, height * 1),
+                  ),
+                ],
+              );
+            } else if (state.hotelListStatus is GetHotelListFailed) {
+              final failureStatus = state.hotelListStatus as GetHotelListFailed;
+              return Center(child: Text('Error: ${failureStatus.message}'));
+            }
+            return Container();
+          },
+        ),
+      ),
+    );
   }
 
-  Widget hotelsListView(List<Accommodation> hotels, double width, double height) {
+  Widget hotelsListView(List<AccommodationDto> hotels, double width, double height) {
     return ListView.builder(
-        padding: const EdgeInsets.all(8.0),
-        itemCount: hotels.length,
-        itemBuilder: (BuildContext context, int index) {
-          var currentHotel = hotels[index];
+      padding: const EdgeInsets.all(8.0),
+      itemCount: hotels.length,
+      itemBuilder: (BuildContext context, int index) {
+        var currentHotel = hotels[index];
 
-          return GestureDetector(
-            child:
-                PlaceCard(
-                    title: currentHotel.title,
-                    image: currentHotel.image,
-                    width: width,
-                    height: height,
-                    isVisited: false,
+        return GestureDetector(
+          child: PlaceCard(
+            title: currentHotel.title,
+            image: currentHotel.image,
+            width: width,
+            height: height,
+            isVisited: false,
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => PlaceDetailsPage(
+                  place: currentHotel,
+                  category: BasePlaceCategory.accommodation,
                 ),
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context)=> PlaceDetailsPage(place: currentHotel, category: BasePlaceCategory.accommodation,)));
-            },
-          );
-        });
+              ),
+            );
+          },
+        );
+      },
+    );
   }
 }
